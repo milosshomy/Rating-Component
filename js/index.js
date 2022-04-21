dd976457d4482e1a164c48f983c7d03a08546c02
Milos Djordjevic <milos.shomy95@hotmail.com>
2022-04-21 20:01:42 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -5,11 +5,8 @@ const select = document.querySelector(".select");
 const box2 = document.querySelector(".box");
 
 rating.forEach((rating) => {
-  console.log(rating);
-
   rating.addEventListener("click", (e) => {
     let number = e.target.value;
-
     submitBtn.addEventListener("click", () => {
       select.innerHTML = `You selected ${number} of 5`;
       box2.style.display = "none";
