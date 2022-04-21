05b145d4143ecd5d69d5522bda479ae4168b1b6f
Milos Djordjevic <milos.shomy95@hotmail.com>
2022-04-21 19:57:22 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
 const rating = document.querySelectorAll(".rating");
 const submitBtn = document.querySelector(".submit-btn");
+const box = document.querySelector(".hide");
+const select = document.querySelector(".select");
+const box2 = document.querySelector(".box");
+
+rating.forEach((rating) => {
+  console.log(rating);
+
+  rating.addEventListener("click", (e) => {
+    let number = e.target.value;
+
+    submitBtn.addEventListener("click", () => {
+      select.innerHTML = `You selected ${number} of 5`;
+      box2.style.display = "none";
+      box.style.display = "block";
+    });
+  });
+});
